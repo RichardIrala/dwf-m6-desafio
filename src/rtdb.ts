61e4c91d3bdcc7dbf8e0fd575cff4d9fcd593d0d
Richard Miguel Irala <richardirala47@hotmail.com>
2022-06-21 13:55:14 -0300

    Se quito la acción de escuchar, y solo se inicializa firebase y se exporta

--- src/rtdb.ts
+++ src/rtdb.ts
@@ -1,11 +1,11 @@
 import firebase from "firebase";
-import lodash from "lodash";
+
 import { state } from "./state";
 
 const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG);
 const app = firebase.initializeApp(firebaseConfig);
 
-const db = firebase.database();
+export const rtdbFrontEnd = firebase.database();
 //Inicializando state
 // state.init();
 // state.subscribe(() => {
@@ -13,15 +13,3 @@ const db = firebase.database();
 // });
 
 //De aca hacia adelante podria incorporarlo al state, así cada vez que se setee un roomRef, se ejecute toda esta línea hasta el final.
-
-const chatroomsRef = db.ref(`/rooms/321321`);
-// console.log(roomLongId);
-
-//Este método indica a chatroomsRef que escuche los cambios en /chatrooms
-chatroomsRef.on("value", (snapshot) => {
-  console.log("puta");
-  const valor = snapshot.val();
-  // console.log(valor);
-  const arrayValor = lodash.map(valor);
-  console.log(arrayValor);
-});
