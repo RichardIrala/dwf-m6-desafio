08e1ac2f7df2ef130634e5eaf0c24239402aa67e
Richard Miguel Irala <richardirala47@hotmail.com>
2022-06-21 13:58:51 -0300

    Se cambiaron propiedades css y del formulario + se utilizan los datos del form con state.joinRoom

--- src/pages/join-room/index.ts
+++ src/pages/join-room/index.ts
@@ -1,4 +1,5 @@
 import { Router } from "@vaadin/router";
+import { state } from "../../state";
 
 const fondoDelJuego = require("url:../../components/imgs/fondo.svg");
 //SEGUIR CON ESTA PAGINA, HACER QUE ESTA PAGINA SE TRANSFORME ENTRE ENTRAR A UN ROOM Y CREAR ROOM
@@ -22,9 +23,9 @@ customElements.define(
         <game-title></game-title>
         <form class="form-new-room">
             
-            <label class="form-new-room__my-name--label">
+            <label class="form-new-room__roomId--label">
                 <span>Id de la sala</span>
-                <input type="number" placeholder="1923" name="my-name" class="form-new-room__myname"/>
+                <input type="number" placeholder="1923" name="roomId" class="form-new-room__myname"/>
                 <span class="form-alert display--none">Necesitas completar todas las casillas</span>
             </label>
 
@@ -55,7 +56,7 @@ customElements.define(
             gap: 20px;
             font-size: 45px;
         }
-        .form-new-room__my-name--label {
+        .form-new-room__roomId--label {
             display: flex;
             flex-direction: column;
             align-items: center;
@@ -94,11 +95,11 @@ customElements.define(
         .querySelector(".form-new-room")
         .addEventListener("submit", (e) => {
           e.preventDefault();
-          if (e.target["my-name"].value != "") {
-            //My-name es el name del input del formulario
-
-            console.log(e.target["my-name"].value);
-            Router.go("/inicio-game");
+          if (e.target["roomId"].value != "") {
+            //roomId es el name del input del formulario
+            const roomId = e.target["roomId"].value;
+            state.joinRoom(roomId);
+            // Router.go("/inicio-game");
           } else {
             //Alerta de que faltan casillas
             const formAlert = this.shadow.querySelectorAll(".display--none");
