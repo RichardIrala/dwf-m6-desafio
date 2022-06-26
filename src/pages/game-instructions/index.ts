b6aad0665a565db90500075f5ff4b986c651d456
Richard Miguel Irala <richardirala47@hotmail.com>
2022-06-26 18:04:54 -0300

    Se eliminaron cosas del viejo juego: SweetAlerts. Se preparo el componente/page  para el juego Online

--- src/pages/game-instructions/index.ts
+++ src/pages/game-instructions/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "@vaadin/router";
 import Swal from "sweetalert2";
+import { state } from "../../state";
 
 const fondoDelJuego = require("url:../../components/imgs/fondo.svg");
 
@@ -19,7 +20,8 @@ customElements.define(
       const div = document.createElement("div");
       div.classList.add("principal-container");
       const style = document.createElement("style");
-      div.innerHTML = ` 
+      div.innerHTML = `
+        <header-in-game-el></header-in-game-el>
         <p class="instructions">Presioná jugar y elegí: piedra, papel o tijera antes de que pasen los 3 segundos.</p>
         <game-button-blue>¡Jugar!</game-button-blue>
         <ppot-el></ppot-el>
@@ -55,38 +57,8 @@ customElements.define(
       this.shadow
         .querySelector("game-button-blue")
         .addEventListener("click", () => {
-          //Aquí estan los colores de los botones de las alertas, para cambiar directamente en la constante y que se apliquen
-          const colorConfirmar = "rgba(0, 144, 72, 1)";
-          const colorDenegar = "rgba(0, 108, 252, 1)";
-          Swal.fire({
-            title: "¿Quieres comenzar a jugar?",
-            confirmButtonText: "Si, quiero continuar",
-            confirmButtonColor: colorConfirmar,
-            showDenyButton: true,
-            denyButtonText: `No, aun no quiero jugar`,
-            denyButtonColor: colorDenegar,
-          }).then((result) => {
-            /* Etapa de confirmación, degenación o click en la pantalla sin responder */
-            if (result.isConfirmed) {
-              Swal.fire({
-                title: "¡A jugar!",
-                showConfirmButton: true,
-                confirmButtonText: "Buenisimo",
-                confirmButtonColor: colorConfirmar,
-              }).then((result) => {
-                if (result) {
-                  //Luego de confirmar y sacar el cartel de "Buenisimo" o darle al boton, lleva directamente al juego
-                  Router.go("/play-game");
-                }
-              });
-            } else if (result.isDenied) {
-              // En caso de darle a la opción NO de la alerta se lanza esta alerta
-              Swal.fire("Cuando estes listo entonces...", "", "warning");
-            } else {
-              //En caso de darle click a la pantalla sin elegir ninguna de las opciones se lanza esta alerta
-              Swal.fire("Necesito que elijas una de las opciones", "", "error");
-            }
-          });
+          state.setReadyToPlay();
+          //AGREGAR el state que consume el fetch que actualiza el estado de jugar a "TRUE", para que luego el juego lo tome y comiencen los chicos a jugar
         });
     }
   }
