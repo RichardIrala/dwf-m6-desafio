ecafeb81f52701f5e0de8b120fcbee4a61209f19
Richard Miguel Irala <richardirala47@hotmail.com>
2022-06-27 19:06:01 -0300

    Se agrego un metodo del state al constructor para setear el start en false de un Jugador. Tmb se cambio visualmente el componente

--- src/components/game-results/index.ts
+++ src/components/game-results/index.ts
@@ -11,6 +11,7 @@ customElements.define(
       state.setRenderResultsOnChange(() => {
         this.render();
       });
+      state.setStartFalse();
     }
     render() {
       this.shadow.innerHTML = ``;
@@ -27,8 +28,12 @@ customElements.define(
       </div>
       <div class="game-results__status">
         <span class="game-results__status-text">Score:</span>
-        <span class="game-results__status-text">${myName}: ${victorias || 0 }</span>
-        <span class="game-results__status-text">${enemyName}: ${derrotas || 0}</span>
+        <span class="game-results__status-text">${myName}: ${
+        victorias || 0
+      }</span>
+        <span class="game-results__status-text">${enemyName}: ${
+        derrotas || 0
+      }</span>
       </div>
       <div class="game-results__volver-a-jugar">
         <game-button-blue class="button-volver-a-jugar">Volver a Jugar</game-button-blue>
@@ -39,30 +44,38 @@ customElements.define(
       //
       const style = document.createElement("style");
       //USAR CONSTANTES PARA LOS COLORES
-      style.innerHTML = `
+      style.innerHTML = /*css*/ `
+        * {
+          box-sizing: border-box;
+          margin: 0;
+        }
+
+        .all-content-container {
+          width: 100vw; height: 100vh;
+          background: linear-gradient(90deg, rgba(6,214,124,1) 0%, rgba(9,67,236,1) 100%);
+          padding: 40px 0;
+          display: flex;
+          gap: 20px;
+          flex-direction: column;
+          justify-content: center; align-items: center;
+        }
+
         .game-results__result-container {
           text-align: center;
-          position: absolute;
-          left: 50%; top: 5%;
-          transform: translate(-50%);
         }
         .game-results__result {
-          font-size: 8vh;
+          font-size: 70px;
           color: white;
         }
         @media (max-width: 500px) {
           .game-results__result {
-            font-size: 5vh;
+            font-size: 45px;
           }
 
         }
         
         .game-results__status {
-          width: 50vw; height: 40vh; background: rgba(0, 144, 72, 1);
-          position: absolute;
-          left: 50%; top: 30%;
-          transform: translate(-50%);
-          margin: auto;
+          background: rgba(0, 144, 72, 1);
           display: flex; flex-direction: column;
           justify-content: center; align-items: center; gap: 2vh;
           border-radius: 4px; border: solid #00ff00 1vh;
@@ -85,21 +98,39 @@ customElements.define(
             border-color: #000000;
           }
         }
+        .game-results__status {
+          width: 300px;
+          padding: 20px;
+        }
+        @media(min-width: 500px) {
+          .game-results__status {
+            width: 400px;
+          }
+        }
+        @media(min-width: 768px) {
+          .game-results__status {
+            width: 600px;
+            padding: 40px 0;
+          }
+        }
+        @media(min-width: 1200px) {
+          .game-results__status {
+            width: 800px;
+            padding: 60px 0;
+          }
+        }
+        
         .game-results__status-text {
           color: white;
-          font-size: 5vh;
+          font-size: 45px;
         }
         @media(max-width: 500px) {
           .game-results__status-text {
           
-            font-size: 3vh;
+            font-size: 32px;
           }
         }
-        .all-content-container {
-          width: 100vw; height: 100vh;
-          background: linear-gradient(90deg, rgba(6,214,124,1) 0%, rgba(9,67,236,1) 100%);
-          
-        }
+        
         .game-results__volver-a-jugar {
           position: absolute;
           left: 50%; bottom: 5%;
